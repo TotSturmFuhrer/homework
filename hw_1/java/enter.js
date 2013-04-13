1c359c1f8e63b0a8a1f60b52a020fa2fe4e69b1a
TotSturmFuhrer <totsturm@mail.ru>
2013-04-13 11:12:44 +0400

    github
    пробный комит

---
+++ hw_1/java/enter.js
@@ -0,0 +1,12 @@
+type="text/javascript"
+function viewdiv(id)
+{
+var el=document.getElementById(id);
+if(el.style.display=="block")
+{
+el.style.display="none";
+} else {
+el.style.display="block";
+}
+}
+			
